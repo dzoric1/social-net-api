ffdf94eb0095e4247b6290e502cac399e9013dd1
dzoric1 <Gerasimov2099@mail.ru>
2024-04-22 21:49:29 +0800

    realized post-controller

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const { UserController } = require('../controllers');
+const PostController = require('../controllers/post-controller');
 const { validateRegister } = require('../utils/validators/userValidator');
 const { authenticateToken } = require('../middleware/auth');
 
@@ -21,5 +22,10 @@ router.post('/login', UserController.login);
 router.get('/users/:id', authenticateToken, UserController.getUserById);
 router.put('/users/:id', authenticateToken, UserController.updateUser);
 router.get('/current', authenticateToken, UserController.current);
+
+router.post('/posts', authenticateToken, PostController.createPost);
+router.get('/posts', authenticateToken, PostController.getAllPosts);
+router.get('/posts/:id', authenticateToken, PostController.getPostById);
+router.delete('/posts/:id', authenticateToken, PostController.deletePost);
 
 module.exports = router;
